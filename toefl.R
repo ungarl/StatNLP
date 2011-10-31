f8c8b7063f1772b81c3c9aead5c09e8b2a847c7a
Adam Kapelner <kapelner@gmail.com>
2011-10-31 16:47:51 -0400

    evaluation

--- toefl.R
+++ toefl.R
@@ -8,7 +8,12 @@
 #
 ##################################################
 
-toefl <- read.csv("data/toefl.csv")
+toefl <- as.data.frame(read.csv("data/toefl.csv"))
+#number of questions and choices
+n_q = nrow(toefl)
+num_choices = 4
+#the answers as a vector of strings
+answers = as.character(toefl$answer)
 
 
 ##################################################
@@ -60,13 +65,16 @@ random.guesser <- function(target, distractors)
 #
 ##################################################
 
-evaluation <- function(guess, answer)
+evaluate <- function(guess_vec)
   {
-    #
-    # insert code here
-    #
+	  sum(guess_vec == answers) / n_q
   }
 
+  evaluate_and_control_for_guesses = function(guess_vec){
+	prop_corr = evaluate(guess_vec)
+	prop_chance = n_q * (1 / num_choices) / n_q
+	(prop_corr - prop_chance) / (1 - prop_chance)
+}
 ###################################################
 #
 #  Main code
@@ -75,6 +83,6 @@ evaluation <- function(guess, answer)
 
 
 guess <- guess.first(toefl$target,toefl[,2:5])
-evaluation(guess, toefl$answer)
-
+evaluate(guess)
+evaluate_and_control_for_guesses(guess)
 
