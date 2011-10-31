624ac0ec47fc625a38e7a3349cf1bf111cc62553
Dean Foster <dean@foster.net>
2011-10-31 16:16:16 -0400

    stub for R

---
+++ toefl.R
@@ -0,0 +1,21 @@
+##################################################
+#
+#   Read in the data
+#
+#   (Note: the data was generated from ruby.
+#   That is the toefl.rb file.
+#
+##################################################
+
+toefl <- read.csv("data/toefl.csv")
+
+
+##################################################
+#
+#  Confirming we got names with the data
+#
+##################################################
+
+names(toefl)
+
+
