2814214e6d6287fb1847c15d7ec52085093b2247
Andy Schwartz <andy.schwartz@gmail.com>
2011-10-31 17:25:42 -0400

    pullVectors seems to be working

--- pullVectors.py
+++ pullVectors.py
@@ -4,11 +4,13 @@
 
 import sys
 import csv
+from optparse import OptionParser  
 
 WORD_COLUMN = 0
 VECTOR_START = 5#column where vector starts
 VECTOR_END = 1000#big number = rest of array
 
+optParser = OptionParser()
 
 optParser.add_option("-t", "--toefl", metavar="FILENAME", dest="toefl",                                                                  
                      help="toefl file")                                 
@@ -22,12 +24,11 @@ optParser = OptionParser()
 print >> sys.stderr, "READING TOEFL FILE"
 reader = csv.reader(open(options.toefl, 'rb'), delimiter=',', quotechar='"')                                                                            
 words = set()
-i = 0;
-for row in reader:                                                                              
+i = 0
+for row in reader:
     if i > 0:
-        for word in row:
-            words.add(word)
-    i +=0
+        words = words.union(set(row))
+    i +=1
 
 print >> sys.stderr, "done."
 
@@ -37,8 +38,18 @@ newRows = {}
 i = 0;
 for row in reader:                                                                                                                                                
     if i > 0:
-        word = row[WORD_COLUN]
+        word = row[WORD_COLUMN]
         if (word in words):
-            newWords[word] = row[VECTOR_START:VECTOR_END]
+            newRows[word] = row[VECTOR_START:VECTOR_END]
     i+=1
 print >> sys.stderr, "done."
+
+print >> sys.stderr, "OUTPUTTING"
+
+words = newRows.keys()
+for word in sorted(words):
+    if word in newRows:
+        print '%s,%s' %(word, ','.join(newRows[word]))
+    else:
+        print >> sys.stderr, "warning %s not found in big dictionary" % word
+                        
