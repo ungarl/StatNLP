3e8921a2dc99f5aeca6bed2b3e392eeaac5643a7
Dean Foster <dean@foster.net>
2011-10-31 16:26:01 -0400

    stubs for R

--- toefl.R
+++ toefl.R
@@ -19,3 +19,61 @@ toefl <- read.csv("data/toefl.csv")
 names(toefl)
 
 
+##################################################
+#
+#  Guess first
+#
+##################################################
+
+guess.first <- function(target, distractors)
+  {
+    return(distractors[,1]);
+  }
+
+
+##################################################
+#
+#  Random guesser
+#
+##################################################
+
+random.guesser <- function(target, distractors)
+  {
+    #
+    # insert code here
+    #
+  }
+
+
+##################################################
+#
+#  Insert your method here
+#
+##################################################
+
+
+
+##################################################
+#
+#  Evaluation
+#
+##################################################
+
+evaluation <- function(guess, answer)
+  {
+    #
+    # insert code here
+    #
+  }
+
+###################################################
+#
+#  Main code
+#
+##################################################
+
+
+guess <- guess.first(toefl$target,toefl[,2:5])
+evaluation(guess, toefl$answer)
+
+
