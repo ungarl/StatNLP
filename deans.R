c7cc76e351995297c60e5b291f9de66da8909878
Dean (jake) <dean@foster.net>
2012-04-23 17:38:08 -0400

    deans version added.

---
+++ deans.R
@@ -0,0 +1,26 @@
+# Stolen from handout
+
+    require("Matrix")
+    require("tensor")
+require("expm")
+
+    c1 <- rexp(3000)*2      # variables we want to find
+    c2 <- rexp(3000)*2
+    theta <- .9           # rotation to "hide the variables"
+    x <- sin(theta)*c1 + cos(theta)*c2
+    y <- sin(theta)*c2 - cos(theta)*c1
+    range <- c(min(c(x,y)),max(c(x,y)))
+    plot(x,y,col="blue",xlim=range,ylim=range)
+
+    data <- cbind(x,y)
+    sigma <- t(data) %*% data
+    
+    eta <- rnorm(2)
+    eta <- eta / norm(as.matrix(eta),"F")
+    tri <- t(data) %*% diag(as.vector(data %*% eta)) %*% data
+    answer <- svd(tri)$u[1,]
+    truth <- c(sin(theta), cos(theta))
+
+     answer
+     truth
+
