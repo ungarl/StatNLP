f0c95b8d35b2cba68549a5d48c198b6732e15d6f
Dean (jake) <dean@foster.net>
2012-04-23 17:53:01 -0400

    better.

--- deans.R
+++ deans.R
@@ -4,19 +4,20 @@
     require("tensor")
 require("expm")
 
-    c1 <- rexp(3000)*2      # variables we want to find
-    c2 <- rexp(3000)*2
+    c1 <- rexp(10000)*2      # variables we want to find
+    c2 <- rexp(10000)*2
     theta <- 0           # rotation to "hide the variables"
-    x <-  cos(theta)*c1 + sin(theta)*c2
-    y <-  sin(theta)*c1 - cos(theta)*c2
+    x <-   cos(theta)*c1 + sin(theta)*c2
+    y <-  -sin(theta)*c1 + cos(theta)*c2
     range <- c(min(c(x,y)),max(c(x,y)))
-    plot(x,y,col="blue",xlim=range,ylim=range)
+#    plot(x,y,col="blue",xlim=range,ylim=range)
     data <- cbind(x,y)
     sigma <- t(data) %*% data
     eta <- rnorm(2)
     eta <- eta / norm(as.matrix(eta),"F")
     tri <- t(data) %*% diag(as.vector(data %*% eta)) %*% data
-    answer <- svd(tri)$u[1,]
+    mysvd <-  svd(tri)
+    answer <- mysvd$u[1,]
     truth <- c(sin(theta), cos(theta))
     answer
     truth
