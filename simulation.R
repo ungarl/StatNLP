41e715bf0526c258082858eb2215c58ff5f32444
Lyle Ungar <ungar@cis.upenn.edu>
2012-04-23 16:40:01 -0400

    add comments

--- simulation.R
+++ simulation.R
@@ -1,9 +1,9 @@
 # Stolen from handout
 
 
-    c1 <- rexp(300)*2
+    c1 <- rexp(300)*2      # variables we want to find
     c2 <- rexp(300)*2
-    theta <- .9
+    theta <- .9           # rotation to "hide the variables"
     x <- sin(theta)*c1 + cos(theta)*c2
     y <- sin(theta)*c2 - cos(theta)*c1
     range <- c(min(c(x,y)),max(c(x,y)))
